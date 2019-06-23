6aea510a73df9e86fdacb58ecc3e7deccfc52b7f
Jhonson Tian <jhonson.tian@yahoo.com>
2019-06-23 15:56:52 +0700

    add homepage screen

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -2,11 +2,13 @@
 import { createStackNavigator } from 'react-navigation';
 import { InputUserName } from '../screens/InputUserName';
 import { InputPassword } from '../screens/InputPassword';
+import { Homepage } from '../screens/Homepage';
 
 const MainStack = createStackNavigator(
   {
     InputUserName,
     InputPassword,
+    Homepage,
   },
   {
     initialRouteName: 'InputUserName',
---
+++ src/screens/Homepage/index.js
@@ -0,0 +1,5 @@
+// @flow
+import Homepage from './Homepage';
+import styles from './styles';
+
+export { Homepage, styles };
