9cd41fe8e572cae6674c8b900ad81ed0bfb6e9cd
Jhonson Tian <jhonson.tian@yahoo.com>
2019-06-23 15:56:14 +0700

    add list of commits api call

--- src/libs/apis.js
+++ src/libs/apis.js
@@ -1,10 +1,10 @@
+// @flow
 import axios from 'axios';
 import { Base64 } from './base64';
 
-
 const BASE_URL = 'https://api.github.com';
 
-export const login = (username, password, callback) => {
+export const login = (username: string, password: string, callback: Function): void => {
   const auth = Base64.encode(`${username}:${password}`);
   axios({
     method: 'get',
@@ -12,6 +12,20 @@ export const login = (username, password, callback) => {
     headers: {
       Accept: 'application/vnd.github.v3+json',
       Authorization: `Basic ${auth}`,
+    },
+  })
+    .then(res => callback(false, res.data))
+    .catch(err => callback(true, err.response));
+};
+
+export const commitsList = (repoName: string, callback: Function): void => {
+  const repoUrl = `${BASE_URL}/repos/${repoName}/commits`;
+  console.log('repo', repoUrl);
+  axios({
+    method: 'get',
+    url: repoUrl,
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
     },
   })
     .then(res => callback(false, res.data))
